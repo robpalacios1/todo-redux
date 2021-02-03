f8842509d054d808e3c88fab0c34c354d226ce4f
roberto <robpalacios1@hotmail.com>
2021-02-03 10:54:30 -0500

    import Input.jsx component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+/***** Components *****/
+import Input from './components/Input'
+
+/***** CSS *****/
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="app">
+      <Input />
     </div>
   );
 }
